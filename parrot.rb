506d3ea14c8404c0d138a9f59854922a4240cfad
meaghan jennings <meaghanljennings@gmail.com>
2020-07-14 17:49:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-phrase = "Pretty bird!"
 def parrot(phrase)
-  parrot("Pretty bird!")
+  parrot(phrase)
   puts phrase
   return phrase
 end
